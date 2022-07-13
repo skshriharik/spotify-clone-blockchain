281dd8d5199e73acdc910e2a7e7c1bc18573cc3b
Shrihari Kulkarni <shrihari.kulkarni@citrix.com>
2022-07-13 17:20:56 +0530

    one time payment done

--- components/login.js
+++ components/login.js
@@ -1,14 +1,19 @@
 import React from 'react'
 import { WalletMultiButton } from '@solana/wallet-adapter-react-ui'
+import { useWallet } from '@solana/wallet-adapter-react'
+import Payments from './Payments'
 
 const styles = {
-    loginPage: ` 
+    loginPage: `flex justify-center	
     flex-col items-center`,
     text: 'text-4xl text-black mb-10',
     selectWallet:  'h-12 w-28 bg-indigo-500 rounded-lg'
 }
 
 export const Login = () => {
+  const wallet = useWallet()
+  if(wallet.connected)
+    return <Payments />
   return (
     <div className={styles.loginPage} >
                 <WalletMultiButton className={styles.selectWallet} />
